ac2654fbc90ad6ee21c10a040c4441f140a2a16a
Gallowan <gallowayjustinj@gmail.com>
2018-09-11 19:04:49 -0700

    Reworked home screen into working RSS feed. Missing header; consider installing ReactNativeElements for easy implementation.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,29 +1,138 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, StatusBar, TouchableOpacity, ImageBackground} from 'react-native';
-import {Actions} from 'react-native-router-flux';
+import React from 'react';
+import { StyleSheet, Text, TextInput, ListView, View, Image, WebView, TouchableOpacity, Alert } from 'react-native';
+//import { SearchBar, Header } from 'react-native-elements';
 
-import Logo from "../components/Logo";
+// In future, set this equal to the default sport set on
+// the user's account. -JG
+const REQUEST_XML_URL = 'http://www.espn.com/espn/rss/ncb/news';
 
-export default class HomePage extends Component {
-    signup() {
-        Actions.signup();
+export default class App extends React.Component {
+
+    // Constructor, involving the state of the RSSFeed
+    // Checks for XML source, loading status
+    constructor(props){
+        super(props);
+        this.state = {
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
+            }),
+            loaded: false,
+            text: REQUEST_XML_URL,
+            title: "",
+        };
     }
+
     render() {
-        return(
-            <View style={styles.container}>
-                <Text>Welcome Home Heaux. We don't need you.</Text>
+        if (!this.state.loaded) {
+            return this.renderLoadingView();
+        }
+
+        return (
+            // Consider installing React Native Elements?
+            <View>
+                {/*<Header*/}
+                {/*leftComponent={{ icon: 'menu', color: '#fff' }}*/}
+                {/*centerComponent={{ text: this.state.title, style: { color: '#fff' } }}*/}
+                {/*rightComponent={{ icon: 'home', color: '#fff' }}*/}
+                {/*/>*/}
+                {/*<SearchBar*/}
+                {/*round*/}
+                {/*onChangeText={(text) => this.setState({text})}*/}
+                {/*onClearText={(text) => this.setState({text})}*/}
+                {/*onEndEditing={ () => this.onPressLearnMore(this.state.text) }*/}
+                {/*placeholder={"Type URL Here..."}*/}
+                {/*/>*/}
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={this.renderFeed}
+                    style={styles.ListView}
+                />
             </View>
-        )
+        );
     }
+
+    componentDidMount() {
+        this.fetchData(REQUEST_XML_URL);
+    }
+
+    reloadURL(NEW_URL) {
+        this.fetchData(NEW_URL);
+    }
+
+    fetchData(URL) {
+        const targetURL = 'https://api.rss2json.com/v1/api.json?rss_url=';
+
+        fetch(targetURL + URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+                console.log(responseData);
+
+                this.setState({
+                    dataSource: this.state.dataSource.cloneWithRows(responseData.items),
+                    loaded: true,
+                    title: responseData.feed.title,
+                });
+            })
+            .done();
+    }
+
+    renderLoadingView() {
+        return (
+            <View style={styles.container}>
+                <Text>
+                    Loading Feed...
+                </Text>
+            </View>
+        );
+    }
+
+    renderFeed(item) {
+        return (
+            <TouchableOpacity onPress={() => {Alert.alert(item.title, item.description)}}>
+                <View style={styles.container}>
+                    <Image
+                        source={{uri: item.enclosure.link}}
+                        style={styles.thumbnail}
+                    />
+                    <View style={styles.rightContainer}>
+                        <Text style={styles.title} numberOfLines={1}>{item.title}</Text>
+                        <Text style={styles.description} numberOfLines={3}>{item.description}</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+
 }
 
 const styles = StyleSheet.create({
     container: {
-        //Ismail this is where you change the background color
-        // FUDGE OFF - Ismail
-        backgroundColor: "#ffffff",
+        paddingTop: 10,
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    thumbnail: {
+        width: 100,
+        height: 70,
+    },
+    rightContainer: {
+        paddingLeft: 5,
+        paddingRight:5,
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    }
+    },
+    title: {
+        fontSize: 16,
+        marginBottom: 4,
+        textAlign: 'left',
+    },
+    description: {
+        fontSize: 12,
+        textAlign: 'left',
+    },
+    listView: {
+        paddingTop: 20,
+        backgroundColor: '#F5FCFF',
+    },
 });
