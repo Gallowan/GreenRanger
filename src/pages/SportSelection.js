97e4bbb0931a81185d47edda6ac53e21ebe777aa
Gallowan <gallowayjustinj@gmail.com>
2018-09-14 23:41:42 -0700

    Fixed buttons on HomePage. Use the notation for further implementation. React-Navigation is having issues and would not install, so instead found out how it worked on Login page. Will use this until we can find the issue with Navigation installation. Buttons now work properly. Added glasses logo to top of homepage.

--- src/pages/SportSelection.js
+++ src/pages/SportSelection.js
@@ -2,15 +2,21 @@ import React from 'react';
 import {Button, View, Text} from 'react-native';
 //import {createStackNavigator} from 'react-navigation';
 import {Component} from "react";
+import {Actions} from 'react-native-router-flux';
 
 export default class SportSelection extends Component {
+    goHome() {
+        Actions.home();
+    }
+
+
     render(){
         return (
             <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
                 <Text>Home Screen</Text>
                 <Button
-                    title="lolbasketbal"
-                    onPress={() => this.props.navigation.navigate('HomePage')}
+                    title="lolbasketball"
+                    onPress={() => this.goHome}
                 />
             </View>
         );
