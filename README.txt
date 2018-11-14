a78e8fb6197a8f07d73557e2e17a6b8803e2c69c
Gallowan <gallowayjustinj@gmail.com>
2018-11-14 12:14:13 -0800

    Added README.

---
+++ README.txt
@@ -0,0 +1,9 @@
+Project GreenRanger
+
+RSS Reading Social Media App - React Native - Expo
+
+A simple app that reads rss feeds and allows users to search for each other and read their feeds. App is NOT COMPLETE.
+
+The application utilizes Google Firebase as its backend and user management. This app is NOT CURRENTLY SET UP.
+
+Program is commented thoroughly.
--- src/pages/PlayerStats.js
+++ src/pages/PlayerStats.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, TextInput, StyleSheet, ListView} from 'react-native';
+import {View, Text, TextInput, StyleSheet, ListView, Image, Platform, SafeAreaView, StatusBar} from 'react-native';
 import {REQUEST_XML_URL} from "./HomePage";
 
 export default class PlayerStats extends React.Component {
@@ -18,53 +18,90 @@ export default class PlayerStats extends React.Component {
 
     render() {
         return (
-            <View>
-                <View style={styles.inner}>
-                    <Text style={styles.t1}> Transcripts </Text> {
-                        this.state.isEditing ?
-                        <TextInput
-                            value={this.state.txt}
-                            onChangeText={(value) => this.setState({ txt: value })}
-                            autoFocus
-                            onBlur={() => this.setState({ isEditing: false })}
-                        /> :
-                        <Text
-                            style={styles.t2}
-                            onPress={() => this.setState({ isEditing: true })}
-                        >
-                            {this.state.txt}
-                        </Text>
-                    }
+            <View style={styles.rowContainer}>
+                <View style={styles.rowText}>
+                    <SafeAreaView/>
+                    <Text style={styles.title} numberOfLines={2} ellipsizeMode ={'tail'}>
+                        {"Transcripts"}
+                    </Text>
+                    <TextInput
+                        style={styles.inputText}
+                        numberOfLines={1}
+                        ellipsizeMode ={'tail'}
+                        value={this.state.txt}
+                        onChangeText={(value) => this.setState({txt: value})}
+                        autoFocus
+                        onPress={() => this.setState({isEditing:true})}
+                        onBlur={() => this.setState({isEditing:false})}>
+                    </TextInput>
                 </View>
             </View>
         )
     }
 }
 
+{/*<View>*/}
+    {/*<View style={styles.inner}>*/}
+        {/*<Text style={styles.t1}> Transcripts </Text> {*/}
+        {/*this.state.isEditing ?*/}
+            {/*<TextInput*/}
+                {/*value={this.state.txt}*/}
+                {/*onChangeText={(value) => this.setState({ txt: value })}*/}
+                {/*autoFocus*/}
+                {/*onBlur={() => this.setState({ isEditing: false })}*/}
+            {/*/> :*/}
+            {/*<Text*/}
+                {/*style={styles.t2}*/}
+                {/*onPress={() => this.setState({ isEditing: true })}*/}
+            {/*>*/}
+                {/*{this.state.txt}*/}
+            {/*</Text>*/}
+    {/*}*/}
+    {/*</View>*/}
+{/*</View>*/}
+
 const styles = StyleSheet.create({
-    t1: {
-        width:300,
-        backgroundColor:"rgba(255,255,255,0.2)",
-        fontSize: 16,
-        color: "#000",
-        marginVertical: 10,
-    },
-    t2: {
-        height:300,
-        width:300,
-        backgroundColor:"rgba(255,255,255,0.2)",
-        borderRadius: 50,
-        paddingHorizontal: 16,
-        fontSize: 16,
-        color: "#000",
-        marginVertical: 10,
-    },
-    inner: {
-        //Ismail this is where you change the background color
-        //backgroundColor: "#455a64",
-        paddingTop: 50,
+    rowContainer: {
         flexGrow: 1,
-        alignItems: "center",
-        justifyContent: "center",
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
+        backgroundColor: '#FFF',
+        marginRight: 10,
+        marginLeft: 10,
+        marginTop: 10,
+        borderRadius: 4,
+        shadowOffset:{  width: 1,  height: 1,  },
+        shadowColor: '#CCC',
+        shadowOpacity: 1.0,
+        shadowRadius: 1,
+        paddingTop: Platform.OS === 'android' ? 25 : 0
+    },
+    title: {
+        paddingLeft: 10,
+        paddingTop: 5,
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#777'
+    },
+    inputText: {
+        paddingLeft: 20,
+        marginTop: 5,
+        fontSize: 14,
+        color: '#777',
+        paddingBottom: 20,
+        flexWrap: 'wrap'
+    },
+    thumbnail: {
+        flex: 1,
+        height: undefined,
+        width: undefined
+    },
+    rowText: {
+        flex: 4,
+        flexDirection: 'column'
+    },
+    safeArea: {
+        flex: 1,
+        backgroundColor: '#ff3b3b'
     },
 });
